2597c6cb42e7e99c8e76949511cdc59822571435
chuliang <chuliang@asiainfo-sec.com>
2016-06-23 21:57:31 +0800

    add mina...

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,8 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+# baidu map AK value
+baidu_AK=L6tV0zhkiNFQnuYIWdIC39wcuHMRR5ML
+
+# DIDI map AK
