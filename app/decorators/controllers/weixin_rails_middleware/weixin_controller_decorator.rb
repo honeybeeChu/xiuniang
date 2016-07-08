e0444b66c5bc5833e5f11a9beb93eefbc5ed392c
chuliang <chuliang@asiainfo-sec.com>
2016-07-08 16:57:06 +0800

    增加Math的引用

--- app/decorators/controllers/weixin_rails_middleware/weixin_controller_decorator.rb
+++ app/decorators/controllers/weixin_rails_middleware/weixin_controller_decorator.rb
@@ -2,8 +2,8 @@
 # 1, @weixin_message: 获取微信所有参数.
 # 2, @weixin_public_account: 如果配置了public_account_class选项,则会返回当前实例,否则返回nil.
 # 3, @keyword: 目前微信只有这三种情况存在关键字: 文本消息, 事件推送, 接收语音识别结果
+include Math
 WeixinRailsMiddleware::WeixinController.class_eval do
-  include Math
 
   @client = WeixinAuthorize::Client.new(ENV["wxa4de3c29bddd316e"], ENV["586589e71887eed25ac77e133778579f"])
 
