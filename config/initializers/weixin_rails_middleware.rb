40d2d3a9e8dc13b6c01d629e423bc89853dd7911
chuliang <chuliang@asiainfo-sec.com>
2017-03-18 15:17:17 +0800

    modify wechat account to xiuniang.

--- config/initializers/weixin_rails_middleware.rb
+++ config/initializers/weixin_rails_middleware.rb
@@ -13,12 +13,12 @@ WeixinRailsMiddleware.configure do |config|
   ## Here configure is for you DON'T WANT TO SAVE your public account into database ##
   # Or the other configure is fit for only one weixin public_account
   # If you config `weixin_token_string`, so it will directly use it
-  config.weixin_token_string = '335a9d6438c4f41fdcf7b789'
+  config.weixin_token_string = '5c1489a85abae7fe504c9c44'
   # using to weixin server url to validate the token can be trusted.
-  config.weixin_secret_string = '6d5dd9526242c753746ae3a8b54affe6'
+  config.weixin_secret_string = '586589e71887eed25ac77e133778579f'
   # 加密配置，如果需要加密，配置以下参数
-  config.encoding_aes_key = 'ef525f6eb0ddf3b1ee8a8d142d40286a94dd199c8ba'
-  config.app_id = "wx9faf547b0e25cf9e"
+  config.encoding_aes_key = '1df38e365b52c4eaadfaeb98f8f1d34c663199e6a16'
+  config.app_id = "wxa4de3c29bddd316e"
 
   ## You can custom your adapter to validate your weixin account ##
   # Wiki https://github.com/lanrion/weixin_rails_middleware/wiki/Custom-Adapter
